90ce456c1b8917556a35a482b4daf4777aff9fb3
jsvargas <js.vargas127@uniandes.edu.co>
2020-03-19 16:46:23 -0500

    Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = 80;
+var port = process.env.PORT || 3000;
 var path = require('path');
 var fs = require('fs');
 var http = require('http');
